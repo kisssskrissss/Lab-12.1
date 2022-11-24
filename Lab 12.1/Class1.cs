8e87f822d75acad3bd09f2978dbc838d1723cd39
krist <krist@LAPTOP-HIQLHLL6>
2022-11-24 15:42:04 +0500

    Добавьте файлы проекта.

---
+++ Lab 12.1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_12
+{
+    static class Func
+
+    {
+        static public double GetL(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetS(double r)
+        {
+            return Math.PI * r * r;
+        }
+        public static bool CheckPoint(double x, double y, double r, double x0, double y0)
+        {
+            return !(Math.Sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0)) >= r);
+        }
+    }
+
+
+}
---
+++ Lab 12.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            double L = Func.GetL(r);
+            double S = Func.GetS(r);
+            bool isCheck = Func.CheckPoint(x, y, r, x0, y0);
+            Console.WriteLine("Длина {0} Площадь {1} Принадлежность окружности: {2}", L, S, isCheck);
+            Console.ReadKey();
+        }
+    }
+}
